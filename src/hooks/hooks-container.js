d03afe3c90a21f071c8121db28ab20dc11f81997
conradoramalho <conrado.ramalho94@gmail.com>
2019-08-05 19:16:01 -0300

    feat: useEffect example

--- src/hooks/hooks-container.js
+++ src/hooks/hooks-container.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const HooksContainer = () => {
   const [stateValue, setValue] = useState(0);
+  const [useEffectValue, setUseEffectValue] = useState();
+
+  useEffect(() => {
+    console.log('useEffect: ');
+    setTimeout(() => setUseEffectValue('useEffectWorked'), 3000);
+  }, [stateValue]);
 
   const incrementValue = () => {
     setValue(stateValue + 1);
@@ -11,13 +17,19 @@ const HooksContainer = () => {
     setValue(stateValue - 1);
   };
 
+  const changeUseEffectValue = () => {
+    setUseEffectValue('some value');
+  };
+
   return (
     <div>
       React hooks
       <br />
       <button onClick={() => incrementValue()}>Increment</button>
       <button onClick={() => decrementValue()}>Decrement</button>
+      <button onClick={() => changeUseEffectValue()}>Decrement</button>
       <b>Local state: {stateValue}</b>
+      <p>Teste: {useEffectValue ? <b>{useEffectValue}</b> : <b>No value</b>}</p>
     </div>
   );
 };
