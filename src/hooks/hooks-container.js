aab75ad8f84ed811ee2053798815b5a62f6c3ab8
conradoramalho <conrado.ramalho94@gmail.com>
2019-08-05 19:25:08 -0300

    feat: useReducer example

--- src/hooks/hooks-container.js
+++ src/hooks/hooks-container.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+import * as Reducer from '../store/hooks_state/hooks_reducer';
+import * as ACTIONS from '../store/actions/actions';
 
 const HooksContainer = () => {
   const [stateValue, setValue] = useState(0);
   const [useEffectValue, setUseEffectValue] = useState();
+  const [state, dispatch] = useReducer(
+    Reducer.HooksReducer,
+    Reducer.initialState
+  );
 
   useEffect(() => {
     console.log('useEffect: ');
@@ -21,15 +27,30 @@ const HooksContainer = () => {
     setUseEffectValue('some value');
   };
 
+  const handleDispatchTrue = () => {
+    dispatch(ACTIONS.success());
+  };
+
+  const handleDispatchFalse = () => {
+    dispatch(ACTIONS.failure());
+  };
+
   return (
     <div>
       React hooks
       <br />
       <button onClick={() => incrementValue()}>Increment</button>
       <button onClick={() => decrementValue()}>Decrement</button>
-      <button onClick={() => changeUseEffectValue()}>Decrement</button>
+      <button onClick={() => changeUseEffectValue()}>Change use effect</button>
+      <br />
       <b>Local state: {stateValue}</b>
+      <br />
       <p>Teste: {useEffectValue ? <b>{useEffectValue}</b> : <b>No value</b>}</p>
+      <br />
+      <button onClick={() => handleDispatchTrue()}>Dispatch true</button>
+      <button onClick={() => handleDispatchFalse()}>Dispatch false</button>
+      <br />
+      <p>reduce value: {state.stateprop1 ? <b>true</b> : <b>false</b>}</p>
     </div>
   );
 };
